fe1c3463259b163d7a7477910740e9635ba7613f
Max <sultonbek.rakhimov@gmail.com>
2023-11-17 16:51:38 +0500

    Initialize simple infra project

---
+++ src/LocalIdentity.SimpleInfra.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace LocalIdentity.SimpleInfra.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
